909ecd584f78164c8060a447511b8b2985557a4b
Eduardocardosodev <eduardocardoso.dev@gmail.com>
2022-12-14 14:50:51 -0300

    ensure AuthUseCase calls TokenGenerator with correct userId

--- src/demain/usecases/auth-usecase.spec.js
+++ src/demain/usecases/auth-usecase.spec.js
@@ -14,6 +14,18 @@ const makeEncrypter = () => {
   return encrypterSpy;
 };
 
+const makeTokenGenerator = () => {
+  class TokenGeneratorSpy {
+    async generate(userId) {
+      this.userId = userId;
+      return this.accessToken;
+    }
+  }
+  const tokenGenaratorSpy = new TokenGeneratorSpy();
+  tokenGenaratorSpy.accessToken = 'any_token';
+  return tokenGenaratorSpy;
+};
+
 const makeLoadUserByEmailRepository = () => {
   class LoadUserByEmailRepositorySpy {
     async load(email) {
@@ -24,6 +36,7 @@ const makeLoadUserByEmailRepository = () => {
 
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
   loadUserByEmailRepositorySpy.user = {
+    id: 'any_id',
     password: 'hashed_password',
   };
   return loadUserByEmailRepositorySpy;
@@ -32,11 +45,17 @@ const makeLoadUserByEmailRepository = () => {
 const makeSut = () => {
   const encrypterSpy = makeEncrypter();
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository();
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy);
+  const tokenGeneratorSpy = makeTokenGenerator();
+  const sut = new AuthUseCase(
+    loadUserByEmailRepositorySpy,
+    encrypterSpy,
+    tokenGeneratorSpy
+  );
   return {
     sut,
     loadUserByEmailRepositorySpy,
     encrypterSpy,
+    tokenGeneratorSpy,
   };
 };
 
@@ -98,5 +117,11 @@ describe('Auth UseCase', () => {
     expect(encrypterSpy.hashedPassword).toBe(
       loadUserByEmailRepositorySpy.user.password
     );
+  });
+
+  test('Should call TokenGenerator with correct userId', async () => {
+    const { sut, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut();
+    await sut.auth('valid_email@mail.com', 'valid_password');
+    expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id);
   });
 });
